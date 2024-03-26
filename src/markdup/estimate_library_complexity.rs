d6cb14a6419aa894665e0e5f4a0d06dda1d00141
Caden Kim <hyperdx1@gmail.com>
2024-03-26 20:54:22 +0900

    m

---
+++ src/markdup/estimate_library_complexity.rs
@@ -0,0 +1,24 @@
+use rust_htslib::bam::{record::RecordExt, Record};
+
+use crate::utils::hash_code;
+
+pub(crate) struct EstimateLibraryComplexity {
+
+}
+
+impl EstimateLibraryComplexity {
+    fn get_read_barcode_value(rec:&Record, tag: &str) -> u64 {
+        if tag.is_empty() {
+            return 0;
+        }
+
+        match rec.get_str_aux(tag.as_bytes()) {
+            Ok(a) => hash_code(a),
+            Err(_) => 0,
+        }
+    }
+
+    fn get_read_one_barcode_value() {
+        
+    }
+}
